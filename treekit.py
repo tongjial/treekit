37310e24c3f27c3e3c033f04a75dfb0d7a2bbb53
Tongjian Liu <molevoliutongjian@gmail.com>
2022-12-12 23:54:56 +0800

    Update treekit.py

--- treekit.py
+++ treekit.py
@@ -19,9 +19,9 @@ The treekit commands are:
     taxa        view taxa summary
     remove      remove specific taxa/tips from trees
     keep        pick-up the trees with specific taxa
-    #mrca        check the monophyly or extract the clade based on given taxa/group
-    #extract     generate subtrees only consist of the given taxa
-    #prune       prune trees with specific branchlength
+    mrca        check the monophyly or extract the clade based on given taxa/group
+    extract     generate subtrees only consist of the given taxa
+    prune       prune trees with specific branchlength
     root        root/reroot/unroot trees
     draw        display trees as textplot
     compare     compare trees between two different sources
